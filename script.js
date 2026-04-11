3260db90abea2a38d23c6ea4f9f626f68f95e632
AWANGARD <zagloxx@gmail.com>
2026-04-11 17:56:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Простое интерактивное действие
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Привет! Это бесплатный сайт, работающий на GitHub Pages.');
+});
